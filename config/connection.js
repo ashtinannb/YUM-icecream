2172a81a365e667ff21b5a92e8ef3cfbba221396
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-06-04 21:45:13 -0500

    testing

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,16 @@ var connection = mysql.createConnection({
   database: "icecream_db"
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '1755',
+      database: 'icecream_db',
+    });
+  }
 
 connection.connect(function(err) {
   if (err) {
--- controllers/icecream_controllers.js
+++ controllers/icecream_controllers.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/icecream.js");
 
-// get route -> index
+
 router.get("/", function(req, res) {
   res.redirect("/icecream");
 });
@@ -33,7 +33,7 @@ router.put("/icecream/:id", function(req, res) {
     // wrapper for orm.js that using MySQL update callback will return a log to console,
     // render back to index with handle
     console.log(result);
-    // Send back response and let page reload from .then in Ajax
+    // Send back response 
     res.sendStatus(200);
   });
 });
