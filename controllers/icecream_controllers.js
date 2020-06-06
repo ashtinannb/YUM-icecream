946b6df91443181e3a1646523a0b45f8f50df33c
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-06-05 19:39:58 -0500

    defined icecreamData

--- controllers/icecream_controllers.js
+++ controllers/icecream_controllers.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res) {
 
 router.get("/icecream", function(req, res) {
   // express callback response by calling burger.selectAllBurger
-  burger.all(function(burgerData) {
+  icecream.all(function(icecreamData) {
     // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
     res.render("index", { icecream_data: icecreamData });
   });
